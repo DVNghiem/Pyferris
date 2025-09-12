c0bf74d661846c1148bcb3d57118b77fcb3dbc54
Dang Van Nghiem <vanghiem848@gmail.com>
2025-09-12 08:45:20 +0700

    chore: update version to 0.5.0 in project files

--- pyferris/__init__.py
+++ pyferris/__init__.py
@@ -2,7 +2,7 @@
 PyFerris - High-performance parallel processing library for Python, powered by Rust and PyO3.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .core import (
     parallel_map, parallel_reduce, parallel_filter, parallel_starmap,
